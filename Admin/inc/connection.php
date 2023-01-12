340608089c190d190d7639c0b29944ca877495ef
nfasss <fasihahayuni01@gmail.com>
2023-01-12 12:30:50 +0800

    update

--- Admin/inc/connection.php
+++ Admin/inc/connection.php
@@ -3,32 +3,28 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "printing";
+$SITEURL = "http://localhost/ONPRINT/";
+
 
 $link = mysqli_connect("localhost", "root") or die(mysqli_connect_error());
 
-	// to select the targeted database
-	mysqli_select_db($link, "testdb") or die(mysqli_error());
-		
-	// to create a query to be executed in sql
-	$query = "insert into user values('$user', '$pass')" 
-		or die(mysqli_connect_error());
-	
-	// to run sql query in database
-	$result = mysqli_query($link, $query);
-	     
-	//Check whether the insert was successful or not
-	if($result) 
-	        {
-		        
-                    echo("Data insert");
-					
-		}
-		else 
-	        {
-			        
-	            die("Insert failed");
-	        }
-	//Close the database connection
-	mysqli_close($link);
+// to select the targeted database
+mysqli_select_db($link, "testdb") or die(mysqli_error());
 
-    
+// to create a query to be executed in sql
+$query = "insert into user values('$user', '$pass')"
+	or die(mysqli_connect_error());
+
+// to run sql query in database
+$result = mysqli_query($link, $query);
+
+//Check whether the insert was successful or not
+if ($result) {
+
+	echo ("Data insert");
+} else {
+
+	die("Insert failed");
+}
+//Close the database connection
+mysqli_close($link);
