c3b870aa6bf6d6064bbefbf5ab799b2c5f14670e
nfasss <fasihahayuni01@gmail.com>
2023-01-26 01:24:33 +0800

    ss

--- Function/feedback.php
+++ Function/feedback.php
@@ -53,10 +53,54 @@
 
 
         }
+
+        .btn-outline-primary {
+            color: #ffffff;
+            background-color: red;
+            border-color: red;
+            border-radius: 1px;
+        }
+
+        .btn-primary {
+            color: #ffffff;
+            background-color: #81BD67;
+            border-color: #81BD67;
+            border-radius: 1px;
+        }
     </style>
 </head>
+<?php
+// to make a connection with database
+$link = mysqli_connect("localhost", "root") or die(mysqli_connect_error());
+//Select the database.
+mysqli_select_db($link, "printing") or die(mysqli_error($link));
+
+//SQL query
+$query = "SELECT * FROM orders ORDER BY OrderID DESC"; // DIsplay the Latest Order at First
+
+//Execute the query (the recordset $rs contains the result)
+$rs = mysqli_query($link, $query);
+
+$sn = 1; //Create a Serial Number and set its initail value as 1
+$count = mysqli_num_rows($rs);
+
+//Close the database connection
+mysqli_close($link);
+
+if ($count > 0) {
+    //Order Available
+    while ($row = mysqli_fetch_assoc($rs)) {
+        //Get all the order details
+        $OrderID = $row['OrderID'];
+        $Feedback = $row['Feedback'];
+        $Complaint = $row['Complaint'];
+    }
+}
+
+?>
 
 <body>
+
     <nav>
         <div class="wrapper">
             <div class="logo"><a href=''>OnPrint.</a></div>
@@ -89,7 +133,7 @@
             <!-- Modal Body-->
             <div class="modal-body text-center">
                 <i class="far fa-file-alt fa-4x mb-3 animated rotateIn icon1"></i>
-                <h3>Your opinion matters</h3>
+                <h3>Your parcel is delivered !</h3>
                 <h5>Help us improve our product? <strong>Give us your feedback.</strong></h5>
                 <hr>
                 <h6>Your Rating</h6>
@@ -97,39 +141,41 @@
 
             <!-- Radio Buttons for Rating-->
             <div class="form-check mb-4">
-                <input name="feedback" type="radio">
+                <?php if ($Feedback == "Very Good") {
+                    echo "selected"; }?><input name='option' type='radio' value='Very Good'>
+                
                 <label class="ml-3">Very good</label>
             </div>
             <div class="form-check mb-4">
-                <input name="feedback" type="radio">
+            <?php if ($Feedback == "Good") {
+                echo "selected"; }?> <input name='option' type='radio' value='Good'>
                 <label class="ml-3">Good</label>
             </div>
             <div class="form-check mb-4">
-                <input name="feedback" type="radio">
-                <label class="ml-3">Mediocre</label>
-            </div>
-            <div class="form-check mb-4">
-                <input name="feedback" type="radio">
+            <?php if ($Feedback == "Bad") {
+                echo "selected"; }?><input name='option' type='radio' value='Bad'>
+           
                 <label class="ml-3">Bad</label>
             </div>
             <div class="form-check mb-4">
-                <input name="feedback" type="radio">
+            <?php if ($Feedback == "Very Bad") {
+                echo "selected"; }?><input name='option' type='radio' value='Bad'>
+           
                 <label class="ml-3">Very Bad</label>
             </div>
 
             <!--Text Message-->
             <div class="text-center">
-                <h4>What could we improve?</h4>
+                <h4>Any complain?</h4>
             </div>
-            <textarea type="textarea" placeholder="Your Message" rows="3"></textarea>
+            <?php echo "<textarea type='textarea' placeholder='   Write Your Message Here' rows='3'></textarea>"?>
 
 
             <!-- Modal Footer-->
             <div class="modal-footer">
                 <a href="" class="btn btn-primary">Send
-                    <i class="fa fa-paper-plane"></i>
                 </a>
-                <a href="" class="btn btn-outline-primary" data-dismiss="modal">Cancel</a>
+                <a href="http://localhost/OnPrint/Function/orderstatus.php" class="btn btn-outline-primary" data-dismiss="modal">Cancel</a>
             </div>
 
         </div>
