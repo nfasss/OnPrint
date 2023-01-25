85a7dae1b1eda521ce5b44b5975eba1240bf6008
t0kwAn <122250547+t0kwAn@users.noreply.github.com>
2023-01-26 00:00:21 +0800

    Add files via upload

---
+++ Function/Order List.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order List</title>
+    <link rel="stylesheet" href="../Style/style.css">
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href=''>OnPrint.</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#courses">Order</a></li>
+                    <li><a href="#tutors">Our Profile</a></li>
+                    <li><a href="#partners">Contact</a></li>
+                    <li><a href="" class="btn-login">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        <!-- untuk home -->
+        <section class="home-header text-center">
+            <div class="container">
+            </div>
+        </section>
+        
+        <div class = "button">
+            <table>
+                <tr>
+                <a href="order.php">ORDER</a>
+</tr>
+                 <tr>
+                 <a href="Order List.php">ORDER LIST</a>
+</tr>
+                <tr>
+                <a href="#home">CHECKOUT</a>
+</tr>
+        </div>
+
+
+        <div class = "table2">
+            <table style = "width:100%">
+            <tr>
+                <th>Order List ID</th>
+                <th>Type of Paper</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Action</th>
+            </tr>
+
+        </table>
+        
+        </section>
+
+
+
+    <div id="footer">
+        <div class="wrapper">
+           <b> Unless explicity stated otherwise,all material is copyright © OnPrint 2022
+    </b> 
+        </div>
+    </div>
+    
+</body>
+</html>
