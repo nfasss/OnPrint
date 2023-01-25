2cff4357bda8ae9ae7be59ff4eaba3354f282f42
Twx00 <122250485+Twx00@users.noreply.github.com>
2023-01-25 18:11:15 +0800

    Update login.php

--- Function/login.php
+++ Function/login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+include("database.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OnPrint</title>
-    <link rel="stylesheet" href="Style/login style.css">
+    <link rel="stylesheet" href="Style/style.css">
 </head>
 
 <header>
@@ -17,7 +21,7 @@
                 <ul>
                     <li><a href="homepage.php">Home</a></li>
                     <li><a href="#courses">Order</a></li>
-                    <li><a href="#tutors">Our Profile</a></li>
+                    <li><a href="Profile_manage.php">Our Profile</a></li>
                     <li><a href="#partners">Contact</a></li>
                     <li><a href="Login.php" class="btn-login">Sign Up</a></li>
                 </ul>
@@ -37,17 +41,29 @@
             <label>User ID:</label><input type="text" name="UserID" placeholder="Enter User ID"><br>
             <label>Password:</label><input type="password" name="UserPassword" placeholder="Enter password"><br>
             <label>User Type:</label>
-            <select id="userType" name="userType">
+            <select id="UserType" name="UserType">
                 <option value="staff">Staff</option>
                 <option value="student">Student</option>
                 <option value="administrator">Administrator</option>
             </select>
             <br><br>
             <input type="button" class="btn1" value="Login" style="padding: 10px 50px;" onclick=" location.href='homepage.php'">
-            <input type="button" class="btn2" value="Register" style="padding: 10px 50px;" onclick=" location.href='register.php'">
+            <input type="button" class="btn2" value="Register" style="padding: 10px 50px;" onclick=" location.href='Register.php'">
         </form>
     </div>
 
+
+    <?php
+    //database post
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+        //$_SESSION("userID") == $_POST("UserID")
+        header("Location: Login.php");
+    }
+
+
+    ?>
     <footer>
         <h4>Unless explicitly stated otherwise, all material is copyright &copy; OnPrint 2022.</h4>
     </footer>
