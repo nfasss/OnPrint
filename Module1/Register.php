e0c2083b3f3b201b1c5082eeddb913dbe7708c2b
Twx00 <122250485+Twx00@users.noreply.github.com>
2023-02-02 22:49:47 +0800

    Update Register.php

--- Module1/Register.php
+++ Module1/Register.php
@@ -19,11 +19,11 @@ include("database.php");
             <div class="logo"><a href=''>OnPrint.</a></div>
             <div class="menu">
                 <ul>
-                    <li><a href="homepage.php">Home</a></li>
-                    <li><a href="#courses">Order</a></li>
-                    <li><a href="Profile_manage.php">Our Profile</a></li>
-                    <li><a href="#partners">Contact</a></li>
-                    <li><a href="Login.php" class="btn-login">Sign Up</a></li>
+                <li><a href="homepage.php">Home</a></li>
+                     <li><a href="order.php">Order</a></li>
+                     <li><a href="Profile_manage.php">Our Profile</a></li>
+                     <li><a href="aboutus.php">About Us</a></li>
+                     <li><a href="Login.php" class="btn-login">Sign Up</a></li>
                 </ul>
             </div>
         </div>
@@ -41,14 +41,16 @@ include("database.php");
         </div>
         <div class="container_L">
             <form class="myForm" action="insertUser_mysql.php" method="post">
+                <label for="name">Password:</label>
+                <input type="password" name="UserPassword" placeholder="Enter Password"><br>
                 <label for="name">First Name:</label>
                 <input type="text" name="UserFirstName" placeholder="Enter First Name"><br>
                 <label for="name">Last Name:</label>
                 <input type="text" name="UserLastName" placeholder="Enter Last Name"><br>
                 <label for="name">Email:</label>
-                <input type="text" name="UserEmail" placeholder="Enter Email"><br>
+                <input type="text" name="UserEmail" placeholder="Ali@gmail.com"><br>
                 <label for="name">Phone Number:</label>
-                <input type="text" name="UserPhoneNum" placeholder="Enter Phone Number"><br>
+                <input type="text" name="UserPhoneNum" placeholder="012-3456789"><br>
                 <label for="name">Address:</label>
                 <input type="text" name="UserAddress" placeholder="Enter Address"><br>
                 <label for="name">State:</label>
@@ -63,7 +65,7 @@ include("database.php");
                 </select>
                 <br>
                 <br>
-                <input type="button" class="btn1" value="Clear" style="padding: 10px 50px;">
+                <input type="button" class="btn1" value="Clear" style="padding: 10px 50px;" onclick=" location.href='register.php'">
                 <input type="submit" class="btn2" value="Submit" style="padding: 10px 50px;">
             </form>
 
